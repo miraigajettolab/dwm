2cba5e0822d18680f648731bea64fe90e1123be5
a <a@gmail.com>
2021-12-25 22:19:06 +0300

    add volume control bindings

--- config.h
+++ config.h
@@ -61,6 +61,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockcmd[] = { "slock", NULL };
+static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "10%+", "unmute", NULL };
+static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "10%-", "unmute", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -74,6 +76,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask, 		XK_l, 	   spawn, 	   {.v = lockcmd } },
+	{ MODKEY,			XK_Right,   spawn,	   {.v = volupcmd } },
+	{ MODKEY,			XK_Left,  spawn,	   {.v = voldowncmd } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
